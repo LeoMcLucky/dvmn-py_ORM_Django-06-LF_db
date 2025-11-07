42933d1213606ace3662a6c43ba1168979c9adbc
Lev <leon.fissenko@gmail.com>
2025-11-07 14:15:23 +0500

    Added related_name for the post field.

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,15 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    title = models.CharField('Заголовок', max_length=200)
+    title = models.CharField(
+        'Заголовок',
+        max_length=200
+    )
     text = models.TextField('Текст')
-    slug = models.SlugField('Название в виде url', max_length=200)
+    slug = models.SlugField(
+        'Название в виде url',
+        max_length=200
+    )
     image = models.ImageField('Картинка')
     published_at = models.DateTimeField('Дата и время публикации')
 
@@ -14,16 +20,19 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         verbose_name='Автор',
-        limit_choices_to={'is_staff': True})
+        limit_choices_to={'is_staff': True}
+    )
     likes = models.ManyToManyField(
         User,
         related_name='liked_posts',
         verbose_name='Кто лайкнул',
-        blank=True)
+        blank=True
+    )
     tags = models.ManyToManyField(
         'Tag',
         related_name='posts',
-        verbose_name='Теги')
+        verbose_name='Теги'
+    )
 
     def __str__(self):
         return self.title
@@ -38,7 +47,11 @@ class Post(models.Model):
 
 
 class Tag(models.Model):
-    title = models.CharField('Тег', max_length=20, unique=True)
+    title = models.CharField(
+        'Тег',
+        max_length=20,
+        unique=True
+    )
 
     def __str__(self):
         return self.title
@@ -59,11 +72,14 @@ class Comment(models.Model):
     post = models.ForeignKey(
         'Post',
         on_delete=models.CASCADE,
-        verbose_name='Пост, к которому написан')
+        verbose_name='Пост, к которому написан',
+        related_name='comments'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Автор')
+        verbose_name='Автор'
+    )
 
     text = models.TextField('Текст комментария')
     published_at = models.DateTimeField('Дата и время публикации')
